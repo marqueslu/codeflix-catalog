eba2cb5159095aa1cb40f0e4fe6948f14f9ae7c5
Lucas Marques <lucsilva@pagar.me>
2023-07-17 08:01:56 -0300

    feat: add ApplicationException

---
+++ src/Codeflix.Catalog.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,8 @@
+namespace Codeflix.Catalog.Application.Exceptions;
+
+public abstract class ApplicationException : Exception
+{
+    public ApplicationException(string? message) : base(message)
+    {
+    }
+}
