68245361f5fe0bbccac8e541f4d199bac41774d9
Lucas Marques <lucsilva@pagar.me>
2023-07-17 08:07:27 -0300

    feat: add GetCategoryInputValidator

---
+++ src/Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInputValidator.cs
@@ -0,0 +1,11 @@
+using FluentValidation;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+
+public class GetCategoryInputValidator : AbstractValidator<GetCategoryInput>
+{
+    public GetCategoryInputValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+    }
+}
