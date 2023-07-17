a384223b44fda773c3024050d69f5e4ea2704c8d
Lucas Marques <lucsilva@pagar.me>
2023-07-17 08:04:49 -0300

    feat: add contract for CreateCategory

---
+++ src/Codeflix.Catalog.Application/UseCases/Category/CreateCategory/ICreateCategory.cs
@@ -0,0 +1,8 @@
+using Codeflix.Catalog.Application.UseCases.Category.Common;
+using MediatR;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+
+public interface ICreateCategory : IRequestHandler<CreateCategoryInput, CategoryModelOutput>
+{
+}
