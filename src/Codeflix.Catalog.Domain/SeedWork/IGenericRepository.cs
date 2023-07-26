30d562f3e229745c52c84acb162b9e7e304318ee
Lucas Marques <lucsilva@pagar.me>
2023-07-26 18:36:52 -0300

    feat: add UpdateAsync method

--- src/Codeflix.Catalog.Domain/SeedWork/IGenericRepository.cs
+++ src/Codeflix.Catalog.Domain/SeedWork/IGenericRepository.cs
@@ -5,4 +5,5 @@ public interface IGenericRepository<TAggregate> : IRepository
     Task InsertAsync(TAggregate aggregate, CancellationToken cancellationToken);
     Task<TAggregate> GetAsync(Guid id, CancellationToken cancellationToken);
     Task DeleteAsync(TAggregate aggregate, CancellationToken cancellationToken);
+    Task UpdateAsync(TAggregate aggregate, CancellationToken cancellationToken);
 }
