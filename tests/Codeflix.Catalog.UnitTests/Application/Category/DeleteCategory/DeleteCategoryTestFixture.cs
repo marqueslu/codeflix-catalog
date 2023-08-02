b9ba5e794b1507d33c0e3bc1cf2fdf781e8f9887
Lucas Marques <lucsilva@pagar.me>
2023-08-02 15:32:08 -0300

    test: add base fixture

---
+++ tests/Codeflix.Catalog.UnitTests/Application/Category/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,13 @@
+using Codeflix.Catalog.UnitTests.Application.Category.Common;
+using Xunit;
+
+namespace Codeflix.Catalog.UnitTests.Application.Category.DeleteCategory;
+
+[CollectionDefinition(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTestFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture>
+{
+}
+
+public class DeleteCategoryTestFixture : CategoryUseCasesBaseFixture
+{
+}
