551d3a17ed5c2e85cacdbe59c2d232fda45b2d9d
Lucas Marques <lucsilva@pagar.me>
2023-08-05 15:03:28 -0300

    test: add base fixture

---
+++ tests/Codeflix.Catalog.IntegrationTests/Base/BaseFixture.cs
@@ -0,0 +1,13 @@
+using Bogus;
+
+namespace Codeflix.Catalog.IntegrationTests.Base;
+
+public class BaseFixture
+{
+    protected Faker Faker { get; set; }
+
+    public BaseFixture()
+    {
+        Faker = new Faker("pt_BR");
+    }
+}
