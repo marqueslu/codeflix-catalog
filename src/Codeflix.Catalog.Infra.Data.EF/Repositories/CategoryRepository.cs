c24a8eab9e01bb214fb73148db968a76672c4fcd
Lucas Marques <lucsilva@pagar.me>
2023-08-26 17:22:04 -0300

    wip

--- src/Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
+++ src/Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -32,7 +32,7 @@ public class CategoryRepository : ICategoryRepository
 
     public Task DeleteAsync(Category aggregate, CancellationToken cancellationToken)
         => Task.FromResult(_categories.Remove(aggregate));
-    
+
     public Task UpdateAsync(Category aggregate, CancellationToken cancellationToken)
         => Task.FromResult(_categories.Update(aggregate));
 
