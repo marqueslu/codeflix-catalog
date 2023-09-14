d9ee3ee2564e84f38188e72655b22bfc70f992a7
Lucas Marques <lucsilva@pagar.me>
2023-09-13 21:57:40 -0300

    chore: add middlewares configuration

---
+++ src/Codeflix.Catalog.Api/Configurations/ControllersConfiguration.cs
@@ -0,0 +1,33 @@
+using Codeflix.Catalog.Api.Filters;
+
+namespace Codeflix.Catalog.Api.Configurations;
+
+public static class ControllersConfiguration
+{
+    public static IServiceCollection AddAndConfigureControllers(this IServiceCollection services)
+    {
+        services.AddControllers(options =>
+            options.Filters.Add(typeof(ApiGlobalExceptionFilter))
+        );
+        services.AddDocumentation();
+        return services;
+    }
+
+    private static IServiceCollection AddDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static WebApplication UseDocumentation(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment()) return app;
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Codeflix"));
+
+        return app;
+    }
+}
