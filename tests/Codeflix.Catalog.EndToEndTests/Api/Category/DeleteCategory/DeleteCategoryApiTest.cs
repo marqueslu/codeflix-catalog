c771e6db7e79dff8ffec4bd391548efb2918a284
Lucas Marques <lucsilva@pagar.me>
2023-09-15 12:13:43 -0300

    test: add DeleteCategory endpoint

---
+++ tests/Codeflix.Catalog.EndToEndTests/Api/Category/DeleteCategory/DeleteCategoryApiTest.cs
@@ -0,0 +1,62 @@
+using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Codeflix.Catalog.EndToEndTests.Api.Category.DeleteCategory;
+
+[Collection(nameof(DeleteCategoryApiTestFixture))]
+public class DeleteCategoryApiTest
+{
+    private readonly DeleteCategoryApiTestFixture _fixture;
+
+    public DeleteCategoryApiTest(DeleteCategoryApiTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(DeleteCategory))]
+    [Trait("EndToEnd/API", "Category/Delete - Endpoints")]
+    public async Task DeleteCategory()
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var exampleCategory = exampleCategoriesList[10];
+
+        var (response, output) = await _fixture
+            .ApiClient
+            .Delete<object>(
+                $"/categories/{exampleCategory.Id}"
+            );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        output.Should().BeNull();
+
+        var dbCategory = await _fixture.Persistence.GetById(exampleCategory.Id);
+        dbCategory.Should().BeNull();
+    }
+
+    [Fact(DisplayName = nameof(ErrorWhenNotFound))]
+    [Trait("EndToEnd/API", "Category/Delete - Endpoints")]
+    public async Task ErrorWhenNotFound()
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var randomGuid = Guid.NewGuid();
+
+        var (response, output) = await _fixture
+            .ApiClient
+            .Delete<ProblemDetails>(
+                $"/categories/{randomGuid}"
+            );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        output!.Status.Should().Be(StatusCodes.Status404NotFound);
+        output.Type.Should().Be("NotFound");
+        output.Title.Should().Be("Not Found");
+        output.Detail.Should().Be($"Category '{randomGuid}' not found.");
+    }
+}
