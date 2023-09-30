46c32506c51c766fd43ca7cc6d79f44b3977096c
Lucas Marques <lucsilva@pagar.me>
2023-09-30 13:53:56 -0300

    test: add date validation

--- tests/Codeflix.Catalog.EndToEndTests/Api/Category/ListCategories/ListCategoriesApiTest.cs
+++ tests/Codeflix.Catalog.EndToEndTests/Api/Category/ListCategories/ListCategoriesApiTest.cs
@@ -1,8 +1,10 @@
 using System.Net;
 using Codeflix.Catalog.Application.UseCases.Category.ListCategories;
 using Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+using Codeflix.Catalog.EndToEndTests.Extensions;
 using FluentAssertions;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Codeflix.Catalog.EndToEndTests.Api.Category.ListCategories;
 
@@ -46,7 +48,7 @@ public class ListCategoriesApiTest : IDisposable
             outputItem.Name.Should().Be(exampleItem.Name);
             outputItem.Description.Should().Be(exampleItem.Description);
             outputItem.IsActive.Should().Be(exampleItem.IsActive);
-            outputItem.CreatedAt.Should().Be(exampleItem.CreatedAt);
+            outputItem.CreatedAt.TrimMilliseconds().Should().Be(exampleItem.CreatedAt.TrimMilliseconds());
         }
     }
 
@@ -99,7 +101,7 @@ public class ListCategoriesApiTest : IDisposable
             outputItem.Name.Should().Be(exampleItem.Name);
             outputItem.Description.Should().Be(exampleItem.Description);
             outputItem.IsActive.Should().Be(exampleItem.IsActive);
-            outputItem.CreatedAt.Should().Be(exampleItem.CreatedAt);
+            outputItem.CreatedAt.TrimMilliseconds().Should().Be(exampleItem.CreatedAt.TrimMilliseconds());
         }
     }
 
@@ -144,11 +146,11 @@ public class ListCategoriesApiTest : IDisposable
             outputItem.Name.Should().Be(exampleItem.Name);
             outputItem.Description.Should().Be(exampleItem.Description);
             outputItem.IsActive.Should().Be(exampleItem.IsActive);
-            outputItem.CreatedAt.Should().Be(exampleItem.CreatedAt);
+            outputItem.CreatedAt.TrimMilliseconds().Should().Be(exampleItem.CreatedAt.TrimMilliseconds());
         }
     }
 
-    [Theory(DisplayName = nameof(SearchByText))]
+    [Theory(DisplayName = nameof(ListByText))]
     [Trait("EndToEnd/API", "Category/List - Endpoints")]
     [InlineData("Action", 1, 5, 1, 1)]
     [InlineData("Horror", 1, 5, 3, 3)]
@@ -158,7 +160,7 @@ public class ListCategoriesApiTest : IDisposable
     [InlineData("Sci-fi", 2, 3, 1, 4)]
     [InlineData("Sci-fi Other", 1, 3, 0, 0)]
     [InlineData("IA", 1, 5, 2, 2)]
-    public async Task SearchByText(
+    public async Task ListByText(
         string search,
         int page,
         int perPage,
@@ -206,20 +208,18 @@ public class ListCategoriesApiTest : IDisposable
             outputItem.Name.Should().Be(exampleItem.Name);
             outputItem.Description.Should().Be(exampleItem.Description);
             outputItem.IsActive.Should().Be(exampleItem.IsActive);
-            outputItem.CreatedAt.Should().Be(exampleItem.CreatedAt);
+            outputItem.CreatedAt.TrimMilliseconds().Should().Be(exampleItem.CreatedAt.TrimMilliseconds());
         }
     }
 
-    [Theory(DisplayName = nameof(SearchOrdered))]
+    [Theory(DisplayName = nameof(ListOrdered))]
     [Trait("EndToEnd/API", "Category/List - Endpoints")]
     [Trait("Integration/Application", "ListCategories - Use Cases")]
     [InlineData("name", "asc")]
     [InlineData("name", "desc")]
     [InlineData("id", "asc")]
     [InlineData("id", "desc")]
-    [InlineData("createdAt", "asc")]
-    [InlineData("createdAt", "desc")]
-    public async Task SearchOrdered(
+    public async Task ListOrdered(
         string orderBy,
         string order
     )
@@ -263,7 +263,64 @@ public class ListCategoriesApiTest : IDisposable
             outputItem.Id.Should().Be(expectedItem.Id);
             outputItem.Description.Should().Be(expectedItem.Description);
             outputItem.IsActive.Should().Be(expectedItem.IsActive);
-            outputItem.CreatedAt.Should().Be(expectedItem.CreatedAt);
+            outputItem.CreatedAt.TrimMilliseconds().Should().Be(expectedItem.CreatedAt.TrimMilliseconds());
+        }
+    }
+
+    [Theory(DisplayName = nameof(ListOrderedDates))]
+    [Trait("EndToEnd/API", "Category/List - Endpoints")]
+    [Trait("Integration/Application", "ListCategories - Use Cases")]
+    [InlineData("createdAt", "asc")]
+    [InlineData("createdAt", "desc")]
+    public async Task ListOrderedDates(
+        string orderBy,
+        string order
+    )
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList();
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var inputOrder = order == "asc" ? SearchOrder.Asc : SearchOrder.Desc;
+        var input = new ListCategoriesInput(
+            page: 1,
+            perPage: 20,
+            sort: orderBy,
+            dir: inputOrder
+        );
+
+        var (response, output) = await _fixture
+            .ApiClient
+            .Get<ListCategoriesOutput>(
+                $"/categories",
+                input
+            );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be(HttpStatusCode.OK);
+        output.Should().NotBeNull();
+        output!.Items.Should().HaveCount(exampleCategoriesList.Count);
+        output.Page.Should().Be(input.Page);
+        output.PerPage.Should().Be(input.PerPage);
+        output.Total.Should().Be(exampleCategoriesList.Count);
+        DateTime? lastItemDate = null;
+        foreach (var outputItem in output.Items)
+        {
+            var exampleItem = exampleCategoriesList
+                .FirstOrDefault(x => x.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            outputItem.Id.Should().Be(exampleItem!.Id);
+            outputItem.Name.Should().Be(exampleItem.Name);
+            outputItem.Description.Should().Be(exampleItem.Description);
+            outputItem.IsActive.Should().Be(exampleItem.IsActive);
+            outputItem.CreatedAt.TrimMilliseconds().Should().Be(exampleItem.CreatedAt.TrimMilliseconds());
+            if (lastItemDate is not null)
+            {
+                if (order is "asc")
+                    Assert.True(outputItem.CreatedAt >= lastItemDate);
+                else
+                    Assert.True(outputItem.CreatedAt <= lastItemDate);
+            }
+
+            lastItemDate = outputItem.CreatedAt;
         }
     }
 
