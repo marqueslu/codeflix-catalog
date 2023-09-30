6552d843de19f29fc6aee40b97c4831d30e923bb
Lucas Marques <lucsilva@pagar.me>
2023-09-30 13:54:19 -0300

    test: adjust sort method

--- tests/Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTestFixture.cs
+++ tests/Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTestFixture.cs
@@ -1,5 +1,8 @@
+using Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
 using Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
 using Xunit;
+using DomainEntity = Codeflix.Catalog.Domain.Entity;
+
 namespace Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.ListCategories;
 
 [CollectionDefinition(nameof(ListCategoriesTestFixture))]
@@ -9,4 +12,22 @@ public class ListCategoriesTextFixtureCollection : ICollectionFixture<ListCatego
 
 public class ListCategoriesTestFixture : CategoryUseCasesBaseFixture
 {
+    public List<DomainEntity.Category> CloneCategoriesListOrdered(IEnumerable<DomainEntity.Category> categoriesList, string orderBy, SearchOrder order)
+    {
+        var listClone = new List<DomainEntity.Category>(categoriesList);
+        var orderedEnumerable = (orderBy.ToLower(), order) switch
+        {
+            ("name", SearchOrder.Asc) => listClone.OrderBy(x => x.Name)
+                .ThenBy(x => x.Id),
+            ("name", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Name)
+                .ThenBy(x => x.Id),
+            ("id", SearchOrder.Asc) => listClone.OrderBy(x => x.Id),
+            ("id", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Id),
+            ("createdat", SearchOrder.Asc) => listClone.OrderBy(x => x.CreatedAt),
+            ("createdat", SearchOrder.Desc) => listClone.OrderByDescending(x => x.CreatedAt),
+            _ => listClone.OrderBy(x => x.Name)
+                .ThenBy(x => x.Id),
+        };
+        return orderedEnumerable.ToList();
+    }
 }
