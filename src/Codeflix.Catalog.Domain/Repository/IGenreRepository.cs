e2966cfdcf9dfadfc3096628eb7f98e8b32189a6
Lucas Marques <lucsilva@pagar.me>
2024-01-13 18:06:11 -0300

    feat: add genre repository contract

---
+++ src/Codeflix.Catalog.Domain/Repository/IGenreRepository.cs
@@ -0,0 +1,8 @@
+using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.SeedWork;
+
+namespace Codeflix.Catalog.Domain.Repository;
+
+public interface IGenreRepository : IGenericRepository<Genre>
+{
+}
